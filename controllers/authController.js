3b2c03818eeced8350e59b1fd6f78c15a4d7eec5
JohelPires <johelpires@gmail.com>
2023-04-03 09:56:25 -0400

    authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require('../models/UserModel')
 
-const register = (req, res) => {
+const register = async (req, res) => {
+  // const user = await User.create({...req.body})
+
   res.status(200).json(req.body)
 }
 module.exports = { register }
